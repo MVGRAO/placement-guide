36c8fe198396455a8ec27b59e943de0a174afd27
MVGRAO <programchewer100@gmail.com>
2026-03-17 12:21:46 +0530

    feat: update favicon to RGUKT branding for improved site identity

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'RGUKT Placement Guide',
   tagline: 'Official placement preparation portal for RGUKT students',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/rgukt.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
